cf59933656c942b85be3043807e73e7137616ef6
harsyax1 <113166639+bubuuble@users.noreply.github.com>
2025-08-12 17:18:11 +0700

    update favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Bimbel App",
+  title: "Bimbel Master",
   description: "Your Learning Partner",
 };
 
